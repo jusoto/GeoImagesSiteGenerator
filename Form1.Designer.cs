8a7edded380f293a7a2dc09356b2c8b10b891a41
jusoto <jusoto@gmail.com>
2016-04-04 22:11:20 -0400

    Origin

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace GeoPhotoSiteGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fbdImages = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtInputFolder = new System.Windows.Forms.TextBox();
+            this.btnImageFolder = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtSiteUrl = new System.Windows.Forms.TextBox();
+            this.txtPageFileName = new System.Windows.Forms.TextBox();
+            this.txtKmlFileName = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pgbFiles = new System.Windows.Forms.ProgressBar();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtApiKey = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtInitialCoordinates = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Site URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Page file name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kml file name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Image Folder";
+            // 
+            // txtInputFolder
+            // 
+            this.txtInputFolder.Location = new System.Drawing.Point(116, 207);
+            this.txtInputFolder.Name = "txtInputFolder";
+            this.txtInputFolder.Size = new System.Drawing.Size(240, 20);
+            this.txtInputFolder.TabIndex = 7;
+            // 
+            // btnImageFolder
+            // 
+            this.btnImageFolder.Location = new System.Drawing.Point(362, 205);
+            this.btnImageFolder.Name = "btnImageFolder";
+            this.btnImageFolder.Size = new System.Drawing.Size(38, 23);
+            this.btnImageFolder.TabIndex = 8;
+            this.btnImageFolder.Text = "...";
+            this.btnImageFolder.UseVisualStyleBackColor = true;
+            this.btnImageFolder.Click += new System.EventHandler(this.btnImageFolder_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(140, 258);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(141, 35);
+            this.btnCreate.TabIndex = 9;
+            this.btnCreate.Text = "Create Site";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtSiteUrl
+            // 
+            this.txtSiteUrl.Location = new System.Drawing.Point(116, 45);
+            this.txtSiteUrl.Name = "txtSiteUrl";
+            this.txtSiteUrl.Size = new System.Drawing.Size(284, 20);
+            this.txtSiteUrl.TabIndex = 10;
+            // 
+            // txtPageFileName
+            // 
+            this.txtPageFileName.Location = new System.Drawing.Point(116, 136);
+            this.txtPageFileName.Name = "txtPageFileName";
+            this.txtPageFileName.Size = new System.Drawing.Size(165, 20);
+            this.txtPageFileName.TabIndex = 11;
+            // 
+            // txtKmlFileName
+            // 
+            this.txtKmlFileName.Location = new System.Drawing.Point(116, 169);
+            this.txtKmlFileName.Name = "txtKmlFileName";
+            this.txtKmlFileName.Size = new System.Drawing.Size(165, 20);
+            this.txtKmlFileName.TabIndex = 12;
+            // 
+            // pgbFiles
+            // 
+            this.pgbFiles.Location = new System.Drawing.Point(24, 308);
+            this.pgbFiles.Name = "pgbFiles";
+            this.pgbFiles.Size = new System.Drawing.Size(376, 21);
+            this.pgbFiles.TabIndex = 13;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(116, 12);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(165, 20);
+            this.txtTitle.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Title";
+            // 
+            // txtApiKey
+            // 
+            this.txtApiKey.Location = new System.Drawing.Point(116, 74);
+            this.txtApiKey.Name = "txtApiKey";
+            this.txtApiKey.Size = new System.Drawing.Size(284, 20);
+            this.txtApiKey.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Google API Key";
+            // 
+            // txtInitialCoordinates
+            // 
+            this.txtInitialCoordinates.Location = new System.Drawing.Point(116, 103);
+            this.txtInitialCoordinates.Name = "txtInitialCoordinates";
+            this.txtInitialCoordinates.Size = new System.Drawing.Size(165, 20);
+            this.txtInitialCoordinates.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Initial Coordinates";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 348);
+            this.Controls.Add(this.txtInitialCoordinates);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtApiKey);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pgbFiles);
+            this.Controls.Add(this.txtKmlFileName);
+            this.Controls.Add(this.txtPageFileName);
+            this.Controls.Add(this.txtSiteUrl);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnImageFolder);
+            this.Controls.Add(this.txtInputFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog fbdImages;
+        private System.Windows.Forms.TextBox txtInputFolder;
+        private System.Windows.Forms.Button btnImageFolder;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtSiteUrl;
+        private System.Windows.Forms.TextBox txtPageFileName;
+        private System.Windows.Forms.TextBox txtKmlFileName;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar pgbFiles;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtApiKey;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtInitialCoordinates;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,493 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeoPhotoSiteGenerator
+{
+    public partial class Form1 : Form
+    {
+        string title = "";
+        string siteUrl = "";
+        string outputFileName = "";
+        string outputFileNameKml = "";
+        string kmlBody = "";
+        string apiKey = "";
+        string initialCoordinates = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtTitle.Text = "Nepal Project";
+            txtSiteUrl.Text = @"http://www.qicfixit.com:8080/nepal";
+            txtPageFileName.Text = "index.html";
+            txtKmlFileName.Text = "points.kml";
+            txtInputFolder.Text = @"C:\Users\Juan\Desktop\TestHtmlGeoTagged\images";
+            txtApiKey.Text = "AIzaSyAhOv2MNrliIM1BrBBOdgzD2Fip1rzUkHQ";
+            txtInitialCoordinates.Text = "28.195258, 83.973013";
+        }
+
+        private void btnImageFolder_Click(object sender, EventArgs e)
+        {
+            showDialogFolder();
+        }
+
+        private void showDialogFolder()
+        {
+            if (fbdImages.ShowDialog() == DialogResult.OK)
+            {
+                txtInputFolder.Text = fbdImages.SelectedPath;
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            title = txtTitle.Text;
+            siteUrl = txtSiteUrl.Text;
+            outputFileName = txtPageFileName.Text;
+            outputFileNameKml = txtKmlFileName.Text;
+            apiKey = txtApiKey.Text;
+            initialCoordinates = txtInitialCoordinates.Text;
+
+            string[] files = Directory.GetFiles(txtInputFolder.Text, "*.jpg");
+
+            string html;
+            string head;
+            string body;
+            string images;
+            string containingFolder;
+            string kml;
+            
+
+            head = getHead();
+            body = getBodyHeader();
+            images = "";
+            containingFolder = getContainingFolder(files[0]);
+
+            foreach (string filePath in files)
+            {
+                images += getImage(filePath, containingFolder);
+                
+            }
+
+            body += images;
+            body += getBodyFooter();
+
+            html = "<html>" + head + body + "</html>";
+
+            kml = getKml();
+
+
+            CreateFile(html, outputFileName);
+            CreateFile(kml, outputFileNameKml);
+        }
+
+        private void CreateFile(string html, string outputFileName)
+        {
+            string outputFilePath = getOutputFilePath(outputFileName);
+            if (File.Exists(outputFilePath))
+            {
+                if(MessageBox.Show("The file " + outputFileName + " already exists. Do you want to overwrite it?", "Confirmation", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    File.Delete(outputFileName);
+                    
+                }
+            }
+            File.WriteAllText(outputFilePath, html);
+        }
+
+        private string getContainingFolder(string filePath)
+        {
+            string containingFolder = "";
+
+            string[] folders = txtInputFolder.Text.Split(Path.DirectorySeparatorChar);
+
+            containingFolder = folders[folders.Length - 1];
+
+            return containingFolder;
+        }
+
+        private string getOutputFilePath(string outputFileName)
+        {
+            string outputFilePath = "";
+
+            string[] folders = txtInputFolder.Text.Split(Path.DirectorySeparatorChar);
+
+            for (int i= 0; i < folders.Length-1; i++)
+            {
+                outputFilePath += folders[i] + Path.DirectorySeparatorChar;
+            }
+
+            outputFilePath += outputFileName;
+
+            return outputFilePath;
+        }
+
+        private string getImage(string filePath, string containingFolder)
+        {
+            string image = "";
+            string fileName;
+            string location = getImageLocation(filePath);
+            string kmlBodyAux = "";
+
+            if (location != "")
+            {
+                string latitude = location.Split(',')[0];
+                string longitude = location.Split(',')[1];
+                string locationKml = longitude + "," + latitude;
+                fileName = Path.GetFileName(filePath);
+
+                image = "<li><a href='#' class='pan-to-marker' data-marker-lat='[latitude]' data-marker-lng='[longitude]' data-content=\"<img src='" + siteUrl + "/[containingFolder]/[name]' title='[title]' height='450'>\">" +@"
+<img src='" + siteUrl + @"/[containingFolder]/thumbs/[name]' title='[title]' height='100'>
+				</a></li>";
+
+                image = image.Replace("[containingFolder]", containingFolder);
+                image = image.Replace("[name]", fileName);
+                image = image.Replace("[title]", fileName);
+                image = image.Replace("[latitude]", latitude);
+                image = image.Replace("[longitude]", longitude);
+                image = image.Replace("[location]", location);
+
+                kmlBodyAux = @"<Placemark>
+      <name>[name]</name>
+      <styleUrl>#style</styleUrl>
+      <ExtendedData>
+        <Data name='image'>
+          <value><![CDATA[<img src='" + siteUrl + @"/[containingFolder]/[name]' title='[title]' height='450'>]]></value>
+        </Data>
+      </ExtendedData>
+      <Point>
+        <coordinates>[locationKml],0</coordinates>
+      </Point>
+    </Placemark>";
+
+                kmlBodyAux = kmlBodyAux.Replace("[containingFolder]", containingFolder);
+                kmlBodyAux = kmlBodyAux.Replace("[name]", fileName);
+                kmlBodyAux = kmlBodyAux.Replace("[title]", fileName);
+                kmlBodyAux = kmlBodyAux.Replace("[locationKml]", locationKml);
+
+            }
+
+            kmlBody += kmlBodyAux;
+
+            return image;
+
+        }
+
+        private string getBodyHeader()
+        {
+            string bodyHead;
+            
+            bodyHead= @"<body>
+  <div class='container'>
+    <div class='pageTitle'>" + title + @"</div>
+      <div>
+        <div id='map'></div>
+        <div id='capture'></div>
+      </div>
+      <div class='container'>
+        <ul class='images'>";
+
+            return bodyHead;
+        }
+
+        private string getBodyFooter()
+        {
+            string bodyFooter;
+
+            bodyFooter = @"      </ul>
+    </div>
+  </div>
+</body>
+";
+
+            return bodyFooter;
+        }
+
+        private string getHead()
+        {
+            //ADD API to Production ?key=AIzaSyAhOv2MNrliIM1BrBBOdgzD2Fip1rzUkHQ
+            string head = @"<head>
+  <title>"+ title + @"</title>
+  <style>
+body {
+	background-color: #ffffff;
+    margin: 0px;
+}
+
+.container {
+	width: 1010px;
+	max-width: 100%;
+	padding-top: 10px;
+    text-align: center;
+}
+
+#map {
+  height: 450px;
+  width: 450px;
+  max-width: 100%;
+  overflow: hidden;
+  float: left;
+  border: thin solid #333;
+  border-right: thick solid #ccc;
+}
+
+#capture {
+  height: 450px;
+  width: 450px;
+  max-width: 100%;
+  overflow: hidden;
+  /*float: left;*/
+  background-color:#ECECFB;
+  border: thin solid #333;
+  border-left: none;
+  text-align:center;
+}
+
+ul.images {
+  margin: 0;
+  padding: 5;
+  white-space: nowrap;
+  width: 1000px;
+  max-width: 100%;
+  overflow-x: auto;
+  background-color: #ddd;
+}
+
+ul.images li {
+  display: inline;
+  width: 150px;
+  height: 150px;
+}
+
+.thumbnail {
+	width: 1px;
+	text-align: center;
+	border: 1px solid #dddddd;
+}
+
+.pageTitle {
+	text-align: center;
+	font-size: 18px;
+	font-weight: bold;
+    padding-bottom: 10px;
+}
+
+.pageDescription {
+	font-size: 12px;
+	line-height: 140%;
+	text-align: justify;
+	padding-left: 0px;
+	padding-right: 0px;
+}
+
+.imageDescription {
+	vertical-align: top;
+	horizontal-align: left;
+	font-size: 11px;
+	line-height: 120%;
+	border: 1px solid #dddddd;
+}
+
+span.imageTitle {
+	font-size: 14px;
+	font-weight: bold;
+}
+
+span.mapLinks {
+	font-family: Arial;
+	font-size: 10px;
+}
+  </style>
+  <script src='https://maps.googleapis.com/maps/api/js[apiKey]'></script>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js'></script>
+  <script>
+    var map;
+    var src = 'http://www.qicfixit.com:8080/nepal/points.kml' + '?rand='+(new Date()).valueOf();
+	
+	$(document).on('click', '.pan-to-marker', function(e) {
+	  e.preventDefault();
+	
+	  var lat, lng;
+	  var $lat = $(this).data('marker-lat');
+	  var $lng = $(this).data('marker-lng');
+	  var $content = $(this).data('content');
+	  lat = $lat;
+	  lng = $lng;
+	  map.setCenter(new google.maps.LatLng(lat, lng));
+	  var testimonial = document.getElementById('capture');
+	  testimonial.innerHTML = $content;
+	});
+
+	function initialize() {
+	  map = new google.maps.Map(document.getElementById('map'), {
+		center: new google.maps.LatLng([initialCoordinates]),
+		zoom: 7,
+		mapTypeId: google.maps.MapTypeId.SATELLITE
+	  });
+	  loadKmlLayer(src, map);
+	}
+
+	function loadKmlLayer(src, map) {
+	  var kmlLayer = new google.maps.KmlLayer(src, {
+	  suppressInfoWindows: true,
+	  preserveViewport: false,
+	  map:map
+	});
+	
+	  google.maps.event.addListener(kmlLayer, 'click', function(event) {
+		var content = event.featureData.infoWindowHtml;
+		var testimonial = document.getElementById('capture');
+		testimonial.innerHTML = content;
+	  });
+	}
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+  </script>
+</head>";
+            head = head.Replace("[apiKey]", "?key="+apiKey);
+            head = head.Replace("[initialCoordinates]", initialCoordinates);
+
+            return head;
+        }
+
+        public string getImageLocation(string filePath)
+        {
+            string location = "";
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            //string item;
+
+            // Create an Image object. 
+            Image image = new Bitmap(@filePath);
+
+            // Get the PropertyItems property from image.
+            PropertyItem[] propItems = image.PropertyItems;
+
+            double latitude = GetLatitude(image);//get Latitude info
+            double longitude = GetLongitude(image);//get Longitude info
+
+            if (latitude > 0 && longitude > 0)
+            {
+                location = latitude + "," + longitude;
+            }
+
+            //Dispose image to free resources
+            image.Dispose();
+
+            return location;
+        }
+
+        //read Latitude info from image file
+        public static double GetLatitude(Image targetImg)
+        {
+            try
+            {
+                //Property Item 0x0001 - PropertyTagGpsLatitudeRef
+                PropertyItem propItemRef = targetImg.GetPropertyItem(1);
+                //Property Item 0x0002 - PropertyTagGpsLatitude
+                PropertyItem propItemLat = targetImg.GetPropertyItem(2);
+                //convert image metadatada properties into latitude
+                return ExifGpsToDouble(propItemRef, propItemLat);
+            }
+            catch (ArgumentException)
+            {
+                //return 0 if there is no GPS properties in image
+                return 0;
+            }
+        }
+
+        public static double GetLongitude(Image targetImg)
+        {
+            try
+            {
+                ///Property Item 0x0003 - PropertyTagGpsLongitudeRef
+                PropertyItem propItemRef = targetImg.GetPropertyItem(3);
+                //Property Item 0x0004 - PropertyTagGpsLongitude
+                PropertyItem propItemLong = targetImg.GetPropertyItem(4);
+                //convert image metadatada properties into longitude
+                return ExifGpsToDouble(propItemRef, propItemLong);
+            }
+            catch (ArgumentException)
+            {
+                //return 0 if there is no GPS properties in image
+                return 0;
+            }
+        }
+
+        //Calculation needed to transform metadata properties into GPS decimal number
+        //source: http://stackoverflow.com/questions/4983766/getting-gps-data-from-an-images-exif-in-c-sharp
+        private static double ExifGpsToDouble(PropertyItem propItemRef, PropertyItem propItem)
+        {
+            uint degreesNumerator = BitConverter.ToUInt32(propItem.Value, 0);
+            uint degreesDenominator = BitConverter.ToUInt32(propItem.Value, 4);
+            float degrees = degreesNumerator / (float)degreesDenominator;
+
+            uint minutesNumerator = BitConverter.ToUInt32(propItem.Value, 8);
+            uint minutesDenominator = BitConverter.ToUInt32(propItem.Value, 12);
+            float minutes = minutesNumerator / (float)minutesDenominator;
+
+            uint secondsNumerator = BitConverter.ToUInt32(propItem.Value, 16);
+            uint secondsDenominator = BitConverter.ToUInt32(propItem.Value, 20);
+            float seconds = secondsNumerator / (float)secondsDenominator;
+
+            float coorditate = degrees + (minutes / 60f) + (seconds / 3600f);
+            string gpsRef = System.Text.Encoding.ASCII.GetString(new byte[1] { propItemRef.Value[0] }); //N, S, E, or W
+            if (gpsRef == "S" || gpsRef == "W")
+                coorditate = 0 - coorditate;
+            return coorditate;
+        }
+
+        private string getKml()
+        {
+            string kmlHead;
+            string kmlFooter;
+            string kml;
+
+
+            kmlHead = getKmlHead();
+            kmlFooter = getKmlFooter();
+
+            kml = kmlHead + kmlBody + kmlFooter;
+
+            return kml;
+
+        }
+
+        private string getKmlFooter()
+        {
+            string kmlFooter = @"  </Document></kml>";
+
+            return kmlFooter;
+        }
+
+        private string getKmlHead()
+        {
+            string kmlHead = @"<?xml version='1.0' encoding='UTF-8'?>
+<kml xmlns = 'http://www.opengis.net/kml/2.2'><Document>
+     <name>KmlFile</name>
+     <Style id='style'>
+        <IconStyle>
+          <Icon>
+            <href>images/pin-icon.png
+            </href>
+          </Icon>
+        </IconStyle>
+        <BalloonStyle>
+          <text>$[image]</text>
+      </BalloonStyle>
+    </Style>";
+
+            return kmlHead;
+        }
+
+
+    }
+}
